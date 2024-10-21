a89ce94700764d284c90a63dcab05b359ce718ca
Ahmed Barazenji <ahmed1079227@gmail.com>
2024-10-21 09:42:04 +0300

    structure made

---
+++ frontend/src/Pages/signin/signin.js
@@ -0,0 +1,11 @@
+import './signin.css';
+
+import React from 'react'
+
+export default function signin() {
+  return (
+    <div>
+      <h2>signin page</h2>
+    </div>
+  )
+}
---
+++ frontend/src/Pages/students/students.js
@@ -0,0 +1,11 @@
+import 'students.css';
+
+import React from 'react'
+
+export default function students() {
+  return (
+    <div>
+      <h2>Students page</h2>
+    </div>
+  )
+}
